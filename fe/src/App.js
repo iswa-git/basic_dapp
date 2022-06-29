ef595847b3d722e3a12e8b8fcdcb3bc02feb4a3e
MDev <ibrikisw@gmail.com>
2022-06-29 23:36:27 +0300

    add / delete on json backend

--- fe/src/App.js
+++ fe/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 //import logo from './logo.svg';
 import './index.css';
 //import Navbar from './components/Navbar';   
@@ -11,78 +11,76 @@ import AddMember from './components/AddMember'
 const App = () => {
   const [ showAddMember, setShowAddMember] = useState (false)
   
-  const [ members, setMember ] = useState ([
-    {
-        "id": 1,
-        "title": "Developer",
-        "name": "Haytham",
-        "netad": ""
-      },
-      {
-        "id": 2,
-        "title": "Developer",
-        "name": "Taha",
-        "netad": ""
-      },
-      {
-        "id": 3,
-        "title":"Developer",
-        "name":"Hesham",
-        "netad": ""
-      },
-      {
-        "id": 4,
-        "title":"Developer",
-        "name":"Ahmad",
-        "netad": ""
-      },
-      {
-        "id": 5,
-        "title":"Developer",
-        "name":"Omar",
-        "netad": ""
-      },
-      {
-        "id": 6,
-        "title":"Developer",
-        "name":"Etaf",
-        "netad": ""
-      },
-      {
-        "id": 7,
-        "title":"Developer",
-        "name":"Mohammad Ali",
-        "netad": ""
-      },
-      {
-        "id": 8,
-        "title":"Developer",
-        "name":"Mohammad",
-        "netad": "0x0A80816cde5C23a7f7e5C6a7B385A1B239C946C2"
-      },
-      {
-        "id": 9,
-        "title":"Manager",
-        "name":"Raghid",
-        "netad": ""
-      },
-      {
-        "id": 10,
-        "title":"Manager",
-        "name":"Devin",
-        "netad": ""
-      }
-])
-// Add  Member
-const addMember = (member) => {
-  const id= Math.floor(Math.random() * 10000 )+ 1
-  const newMember = {id, ...member}
-  
-  setMember([...members, newMember])
+  const [ members, setMember ] = useState ([])
+
+
+  useEffect (()=> {
+    const getMembers = async () => {
+      const membersFromServer = await fetchMembers()
+      setMember(membersFromServer)
+    }
+
+    getMembers()
+  }, [])
+
+// Fetch Members
+const fetchMembers = async () => {
+  const res = await fetch('http://localhost:5000/team')
+  const data = await res.json()
+
+  return data
 }
+
+
+  // Add Task
+  //const addTask = async (task) => {
+    // const res = await fetch('http://localhost:5000/tasks', {
+    //   method: 'POST',
+    //   headers: {
+    //     'Content-type': 'application/json',
+    //   },
+    //   body: JSON.stringify(task),
+    // })
+
+    // const data = await res.json()
+
+    // setTasks([...tasks, data])
+
+    // const id = Math.floor(Math.random() * 10000) + 1
+    // const newTask = { id, ...task }
+    // setTasks([...tasks, newTask])
+  // }
+
+// Add  Member
+const addMember = async (member) => {
+  const res = await fetch('http://localhost:5000/team/', {
+    method: 'POST',
+    headers: {
+      'Content-type':'application/json'
+    },
+    body: JSON.stringify(member)
+  })
+
+  const data = await res.json()
+  setMember([...member, data])
+  // const id= Math.floor(Math.random() * 10000 )+ 1
+  // const newMember = {id, ...member}
+  
+  // setMember([...members, newMember])
+}
+
 // Delete Member
-const deleteMember = (id) => {
-  setMember (members.filter((member) => member.id !== id))
+const deleteMember = async (id) => {
+  const res = await fetch (`http://localhost:5000/team/${id}`, {
+    method: 'DELETE',
+  })
+  console.log(res)
+
+   //We should control the response status to decide if we will change the state or not.
+   res.status === 200
+   ?  setMember (members.filter((member) => member.id !== id))
+   : alert('Error Deleting This Member')
+
 }
     return (
       <div className="container">
